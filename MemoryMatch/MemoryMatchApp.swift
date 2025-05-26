44d575f21c9c318fc3f991ad7f4c42b9f2c6c070
Aishwarya <aishoi232002@gmail.com>
2025-05-26 10:08:42 +0530

    Initial Commit

---
+++ MemoryMatch/MemoryMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchApp.swift
+//  MemoryMatch
+//
+//  Created by Aishwarya Kannan on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemoryMatchTests/MemoryMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMatchTests.swift
+//  MemoryMatchTests
+//
+//  Created by Aishwarya Kannan on 26/05/25.
+//
+
+import Testing
+@testable import MemoryMatch
+
+struct MemoryMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
